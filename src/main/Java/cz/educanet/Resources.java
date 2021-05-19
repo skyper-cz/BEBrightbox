af456ee09ca35e2dd7bba55cd725adbe23f0a8fe
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-05-19 11:06:27 +0200

    backup patch

--- src/main/Java/cz/educanet/Resources.java
+++ src/main/Java/cz/educanet/Resources.java
@@ -8,9 +8,10 @@ import javax.ws.rs.core.Response;
 public class Resources {
 
     @GET
+    @Path("data")
     public Response getPrices() {
         try {
-            return Response.ok(JsonLoader.loadJson("data.json")).build();
+            return Response.ok(JsonLoader.loadJson("./data.json")).build();
         } catch (Exception e) {
             return Response.status(500, "Soubor nenalezen.").build();
         }
